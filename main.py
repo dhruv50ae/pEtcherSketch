d8040d76580af21e5fe55aca32e09f0c65a804e4
dhruv50ae <linkdp2012@gmail.com>
2022-07-13 13:21:06 +0100

    WASD mini project

--- main.py
+++ main.py
@@ -6,6 +6,28 @@ screen = Screen()
 def moveFowards():
     rammus.forward(10)
 
+def moveBackwards():
+    rammus.backward(10)
+
+def turnLeft():
+    newHeading = rammus.heading()+10
+    rammus.setheading(newHeading)
+
+def turnRight():
+    newHeading = rammus.heading()-10
+    rammus.setheading(newHeading)
+
+def clear():
+    rammus.clear()
+    rammus.penup()
+    rammus.home()
+    rammus.pendown()
+
+
 screen.listen()
-screen.onkey(key="space", fun=moveFowards)
+screen.onkey(key="w", fun=moveFowards)
+screen.onkey(key="s", fun=moveBackwards)
+screen.onkey(key="a", fun=turnLeft)
+screen.onkey(key="d", fun=turnRight)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
